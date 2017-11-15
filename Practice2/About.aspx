3e5c588b7e9f8d9d182726a6b4f5588a6b0b7ec3
Robert Bourne <robert.bourne@redvector1.com>
2017-11-15 10:14:21 -0500

    Playing around with page lifecycle

--- Practice2/About.aspx
+++ Practice2/About.aspx
@@ -1,5 +1,18 @@
 ﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="Practice2.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    
+    <h2>Page Life Cycle</h2>
+    <p>
+        <asp:Label ID="labeInit" runat="server" />
+    </p>
+    <p>
+        <asp:Label ID="lblPageLoad" runat="server" />
+    </p>
+    <p>
+        <asp:Label ID="lblPostBack" runat="server" />
+    </p>
+    <p>
+        <asp:Label ID="lblButtonEvent" runat="server" />
+    </p>
+    <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
 </asp:Content>
--- Practice2/About.aspx.cs
+++ Practice2/About.aspx.cs
@@ -9,9 +9,55 @@ namespace Practice2
 {
     public partial class About : Page
     {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            bool isPostBack = Page.IsPostBack;
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            labeInit.Text = "Text Set During Init";
+        }
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            bool isPostBack = Page.IsPostBack;
+        }
         protected void Page_Load(object sender, EventArgs e)
+        {
+            lblPageLoad.Text = "Text set during page load";
+
+            if(Page.IsPostBack)
+            {
+                lblPostBack.Text = "Info has been Posted Back";
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
         {
 
         }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
